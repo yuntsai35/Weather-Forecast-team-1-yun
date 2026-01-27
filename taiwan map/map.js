418e2d53f4e7c191f8a2eb77ca67793d85f960bc
pervertcoder <lance60702@gmail.com>
2026-01-27 13:43:09 +0800

    1/27 map layout update

--- taiwan map/map.js
+++ taiwan map/map.js
@@ -1,13 +1,15 @@
 "use strict";
 
-const svg = d3.select("svg");
+const svg = d3.select(".svg__big");
 const g = svg.append("g");
 const width = +svg.attr("width");
 const height = +svg.attr("height");
 
 d3.json("./map_data/COUNTY_MOI_1140317.json").then((data) => {
   console.log(data);
-  const testData = data.features[0];
+  // const testData = data.features[0];
+  data.features.splice(0, 1);
+  data.features.splice(12, 1);
 
   const projection = d3
     .geoIdentity()
@@ -25,5 +27,5 @@ d3.json("./map_data/COUNTY_MOI_1140317.json").then((data) => {
     .append("title")
     .text((d) => d.properties.COUNTYNAME); // tooltip
 
-  g.attr("transform", "translate(-500, 30) scale(1)");
+  g.attr("transform", "translate(-600, 1) scale(1.2)");
 });
