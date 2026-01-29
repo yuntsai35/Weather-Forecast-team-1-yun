b1b49db238a30fd6f4fc01f178104aacfeaa60fb
Eden <1denchen.work@gmail.com>
2026-01-29 13:36:40 +0800

    fix: resolve broken 'Send to Discord' button

--- page/script/webhook.js
+++ page/script/webhook.js
@@ -1,28 +1,31 @@
-const discord=document.querySelector('#discord-btn');
+const discord = document.querySelector("#discord-btn");
 discord.addEventListener("click", async function () {
-    const cityText = document.querySelector('#city-btn .dropdown__text').textContent;
-    const areaText = document.querySelector('#area-btn .dropdown__text').textContent;
-    
-    if(cityText === '選擇縣市'){
-        alert('請選擇縣市');
-        return
-    }
-    if(areaText === '選擇鄉鎮市區'){
-        alert('請選擇鄉鎮市區');
-        return
-    }
-   
-    let response=await fetch("/sendWebhook",{
-        method:"POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body:JSON.stringify({cityText: cityText, areaText:areaText})
-    });
-    let result = await response.json();
-    
-    if(response.ok){
-        alert('成功發送資料到Discord !');
-    }
-})
+  const cityText = document.querySelector(
+    "#county-btn .dropdown__text"
+  ).textContent;
+  const areaText = document.querySelector(
+    "#area-btn .dropdown__text"
+  ).textContent;
 
+  if (cityText === "選擇縣市") {
+    alert("請選擇縣市");
+    return;
+  }
+  if (areaText === "選擇鄉鎮市區") {
+    alert("請選擇鄉鎮市區");
+    return;
+  }
+
+  let response = await fetch("/sendWebhook", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ cityText: cityText, areaText: areaText }),
+  });
+  let result = await response.json();
+
+  if (response.ok) {
+    alert("成功發送資料到Discord !");
+  }
+});
